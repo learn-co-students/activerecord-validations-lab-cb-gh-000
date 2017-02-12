f5fbb381a6964b6befb85f1eeccf6de4250b238a
Mondo Gao <mondogao@gmail.com>
2017-02-12 16:05:23 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
+class ClickbaitValidator < ActiveModel::Validator
+  def validate(record)
+    unless record.title && record.title.match(/Won\'t Believe|Secret|Top [0-9]|Guess/)
+      record.errors[:title] << "Clickbait!"
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validates :title, clickbait: true
 end
